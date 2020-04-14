bfc2b7af3dde672c599e49189565c067ac875948
David Blasco <dbp038bn@gmail.com>
2020-04-14 17:12:51 +0200

    Fixed vertex shader not transforming normals

--- src/gfx/shader/PerspectiveVertexShader.h
+++ src/gfx/shader/PerspectiveVertexShader.h
@@ -8,6 +8,9 @@ struct PerspectiveVertexShader {
 	Output operator()( const Input &vtx ) {
 		Output vsOut = vtx;
 		vsOut.position = transformMatrix * vsOut.position;
+		Vector4f normal( vsOut.normal.x(), vsOut.normal.y(), vsOut.normal.z(), 0.0f );
+		normal = transformMatrix * normal;
+		vsOut.normal = Vector3f( normal.x(), normal.y(), normal.z() );
 		vsOut.position = perspectiveMatrix * vsOut.position;
 		return vsOut;
 	}
