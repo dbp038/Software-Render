1782edcd2fc0901202a4a712852b908c6667405d
David Blasco <dbp038bn@gmail.com>
2020-04-18 14:29:05 +0200

    Uploaded missing models and textures & updated test scene

--- src/game/ExampleScene.cpp
+++ src/game/ExampleScene.cpp
@@ -1,69 +1,61 @@
 #include "pch.h"
 #include "ExampleScene.h"
-#include "GeometryGenerator.h"
 #include "app/Application.h"
+#include "app/resources/FileLoader.h"
 #include "gfx/context/Viewport.h"
 
-#include "gfx/context/TextureBitmap.h"
-#include "gfx/context/VertexDeclarations.h"
 #include "gfx/context/TextureSampler.h"
 #include "gfx/shader/PerspectiveVertexShader.h"
-#include "gfx/shader/FaceColorGeometryShader.h"
-#include "gfx/shader/FaceNormalsGeometryShader.h"
-#include "gfx/shader/TexturePixelShader.h"
-#include "gfx/shader/VertexColorPixelShader.h"
 #include "gfx/shader/BasicIlluminationPixelShader.h"
 
-using VertexType = PosUvVertex;
+using VertexType = PosNUvVertex;
+
 using Sampler = TextureLinearSampler;
 
 using VS = PerspectiveVertexShader<VertexType, VertexType>;
-using GS = FaceNormalsGeometryShader;
-using PS = BasicIlluminationPixelShader<TextureLinearSampler>;
-//using PS = TexturePixelShader<VertexType, Sampler>;
+using PS = BasicIlluminationTexturePixelShader<Sampler>;
 
-using CtxType = RenderContext<
-	VertexType,
-	VS,
-	PS,
-	GS
->;
+using CtxType = RenderContext<VertexType, VS, PS>;
+
+Model<VertexType> model;
 
 ExampleScene::ExampleScene() : winTitleUpdater( 1.0f / 23.0f ) {
 	int width = App.GetWindow().GetWidth();
 	int height = App.GetWindow().GetHeight();
 
-	auto vertices = GeometryGenerator::Cube::GetWrapedVertices( 1.0f );
-	auto colors = GeometryGenerator::Cube::GetColors();
-	auto uvs = GeometryGenerator::Cube::GenerateWrapedUVs();
-	
-	std::vector<VertexType> cubeVertices;
-	auto cubeIndices = GeometryGenerator::Cube::GetWrapedIndices();
-	for ( size_t i = 0; i < vertices.size(); i++ ) {
-		cubeVertices.push_back( VertexType{ vertices[ i ], uvs[ i ] } );
-	}
-
+	// initialize render context
 	pCtx = std::make_unique<CtxType>();
 	CtxType &ctx = *static_cast<CtxType *>( pCtx.get() );
 
+	// pass texture to pixel shader
 	auto &ps = ctx.GetPixelShaderData();
-	ps.texture.SetTexture( "bin\\resources\\box_hd.png" );
+	ps.texture.SetTexture( "bin\\resources\\textures\\skull.png" );
 	ps.sampler.SetUVMode( ITextureSampler::UVMode::WRAP );
 
 	ctx.BindViewport( Viewport( width, height ) );
 
-	ctx.BindVertexData( cubeVertices );
-	ctx.BindIndexData( cubeIndices );
+	// load model and pass it to render context
+	model = FileLoader::LoadPosNUvModelFromFile( "bin\\resources\\models\\skull.obj" );
+	ctx.BindVertexData( model.vertices );
+	ctx.BindIndexData( model.indices );
 
-	Matrix4f perspecive = Matrices::PerspectiveLH( 1.0f, float( height ) / width, 0.5f, 1000.0f );
+	Matrix4f perspecive = Matrices::PerspectiveFovLH( PI / 3.0f, float( width ) / height, 0.5f, 1000.0f );
 	ctx.BindVertexShaderBuffer( { Matrix4f::Identity(), perspecive } );
+
+	ctx.BindDepthBuffer( width, height );
+
+	screenSize = Vector2f( width, height );
 }
 
 void ExampleScene::Update() {
 	auto &kb = Keyboard::Get();
 	float speed = rotationSpeed;
 	if ( kb.IsKeyDown( Keyboard::LeftShift ) )
-		speed *= 2;
+		speed *= 2.0f;
+	if ( kb.IsKeyDown( Keyboard::LeftControl ) )
+		speed *= 0.5f;
+
+	// rotate model
 	if ( kb.IsKeyDown( Keyboard::W ) ) {
 		pitch -= Time.delta * speed;
 	}
@@ -83,22 +75,43 @@ void ExampleScene::Update() {
 		roll += Time.delta * speed;
 	}
 
+	// rotate light direction
 	if ( kb.IsKeyDown( Keyboard::I ) ) {
-		yOffset += Time.delta * speed;
+		lightPitch += Time.delta * speed;
 	}
 	if ( kb.IsKeyDown( Keyboard::K ) ) {
-		yOffset -= Time.delta * speed;
+		lightPitch -= Time.delta * speed;
 	}
 	if ( kb.IsKeyDown( Keyboard::L ) ) {
-		xOffset += Time.delta * speed;
+		lightYaw += Time.delta * speed;
 	}
 	if ( kb.IsKeyDown( Keyboard::J ) ) {
-		xOffset -= Time.delta * speed;
+		lightYaw -= Time.delta * speed;
 	}
 	if ( kb.IsKeyDown( Keyboard::U ) ) {
-		zOffset += Time.delta * speed;
+		lightRoll += Time.delta * speed;
 	}
 	if ( kb.IsKeyDown( Keyboard::O ) ) {
+		lightRoll -= Time.delta * speed;
+	}
+
+	// translate model
+	if ( kb.IsKeyDown( Keyboard::NumPad8 ) ) {
+		yOffset += Time.delta * speed;
+	}
+	if ( kb.IsKeyDown( Keyboard::NumPad2 ) ) {
+		yOffset -= Time.delta * speed;
+	}
+	if ( kb.IsKeyDown( Keyboard::NumPad6 ) ) {
+		xOffset += Time.delta * speed;
+	}
+	if ( kb.IsKeyDown( Keyboard::NumPad4 ) ) {
+		xOffset -= Time.delta * speed;
+	}
+	if ( kb.IsKeyDown( Keyboard::NumPad5 ) ) {
+		zOffset += Time.delta * speed;
+	}
+	if ( kb.IsKeyDown( Keyboard::NumPad0 ) ) {
 		zOffset -= Time.delta * speed;
 	}
 
@@ -112,13 +125,25 @@ void ExampleScene::Draw( Graphics &gfx ) {
 	gfx.ClearBackground();
 
 	CtxType &ctx = *static_cast<CtxType *>( pCtx.get() );
+	ctx.ClearDepthBuffer();
 
+	// apply transformations to model
 	auto &psBuffer = ctx.GetVertexShaderData();
-	psBuffer.transformMatrix = Matrices::RotationYawPitchRoll4f( yaw, pitch, roll );
-	psBuffer.transformMatrix = Matrices::Translation4f( xOffset, yOffset, zOffset + 3.0f ).inverse() * psBuffer.transformMatrix;
+	psBuffer.transformMatrix = Matrices::RotationYawPitchRoll4f( yaw, pitch, roll ) * Matrices::Scale4f(1.0f);
+	psBuffer.transformMatrix = Matrices::Translation4f( xOffset, yOffset, zOffset + 2.0f ) * psBuffer.transformMatrix;
 	ctx.BindVertexShaderBuffer( psBuffer ); // not technically needed
 
+	// apply transformations to light
+	auto &ps = ctx.GetPixelShaderData();
+	Vector4f lightDir;
+	lightDir = Matrices::RotationYawPitchRoll4f( lightYaw, lightPitch, lightRoll ) * Vector4f( 0.0f, 0.0f, 1.0f, 1.0f );
+	ps.lightDirection.x() = lightDir.x();
+	ps.lightDirection.y() = lightDir.y();
+	ps.lightDirection.z() = lightDir.z();
+
+	// tell graphics what we want to draw passing the render context with all this info
 	gfx.BindContext( ctx );
+	// perform the actual draw
 	gfx.Draw();
 }
 
